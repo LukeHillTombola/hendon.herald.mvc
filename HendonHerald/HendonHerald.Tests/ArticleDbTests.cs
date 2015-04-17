81641227f2987f768255c3de39f20dc4355f3276
unknown <Luke.Hill@tombola-0629.tombola.emea>
2015-04-17 14:45:16 +0100

    Added NUnit to project. Set up CRUD integration test for ArticleDbContext

---
+++ HendonHerald/HendonHerald.Tests/ArticleDbTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using HendonHerald.DAL.Repositories;
+using HendonHerald.DAL.Entities;
+
+namespace HendonHerald.Tests
+{
+	[TestFixture]
+	public class ArticleDbTests
+	{
+		[Test]
+		public void CRUD_Integration_Test()
+		{
+			GenericUnitOfWork unitOfWork = new GenericUnitOfWork();
+			Article article = new Article();
+
+			article.Title = "Test article";
+			article.Body = "Test body";
+
+			unitOfWork.Repository<Article>().Add(article);
+			unitOfWork.SaveChanges();
+
+			Article retrievedArticle = unitOfWork.Repository<Article>().GetSingle(s => s.Id == article.Id);
+
+			Assert.AreEqual(article, retrievedArticle);
+
+			retrievedArticle.Body = "Updated test body";
+
+			unitOfWork.Repository<Article>().Update(retrievedArticle);
+
+			unitOfWork.SaveChanges();
+
+			retrievedArticle = unitOfWork.Repository<Article>().GetSingle(s => s.Id == article.Id);
+
+			Assert.AreEqual("Updated test body", retrievedArticle.Body);
+
+			unitOfWork.Repository<Article>().Delete(retrievedArticle);
+			unitOfWork.SaveChanges();
+
+			retrievedArticle = unitOfWork.Repository<Article>().GetSingle(s => s.Id == article.Id);
+
+			Assert.IsNull(retrievedArticle);
+
+		}
+	}
+}
